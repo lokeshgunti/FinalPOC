e2b3780cd354b33f45ecfc1e4b07e5de967e70e4
lokeshgunti <lokesh.gunti@zafin.com>
2017-06-23 10:07:59 +0530

    v1

---
+++ src/test/java/testNG/NewTest.java
@@ -0,0 +1,32 @@
+package testNG;
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.ChromeDriver;
+	
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest 
+{		
+	    private WebDriver driver;	
+	    
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.chrome.driver", "D:\\ADS40-As on 01-Dec-2016\\Trainings\\Selenium Training by Bolla & Smitha\\Softwars\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();  
+		}
+		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://newtours.demoaut.com/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Welcome: Mercury Tours")); 		
+		}
+		
+		@AfterTest
+		public void afterTest() {
+			//driver.quit();			
+		}		
+		}
